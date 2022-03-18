69ff2de26eb7c4bff9b9291e0e496cf56a085ff4
ivlev.nikit <ivlev.nikit@gmail.com>
2022-03-19 02:49:02 +0300

    fix

--- bank.cpp
+++ bank.cpp
@@ -1,16 +1,32 @@
 #include "bank.h"
 #include <stdexcept>
 namespace bank {
+
 user::user(std::string const &name_) : username(name_) {
     user_balance_xts = 100;
     std::string message = "Initial deposit for " + name_;
     transaction temp(nullptr, 100, message);
     transtactions.push_back(temp);
 }
+
+user_transactions_iterator user::monitor() const {
+    return user_transactions_iterator(this);
+}
+user_transactions_iterator::user_transactions_iterator(const user *user_ptr)
+    : user_ptr(user_ptr), index(user_ptr->transtactions.size()) {
+}
+
+transaction user_transactions_iterator::wait_next_transaction() {
+    std::unique_lock l(user_ptr->m_m);
+    while (index >= user_ptr->transtactions.size()) {
+        user_ptr->cond.wait(l);
+    }
+    return user_ptr->transtactions[index++];
+}
 void user::transfer(user &counterparty,
                     int amount_xts,
                     const std::string &comment) {
-    std::unique_lock l(m_m);
+    std::scoped_lock l(m_m, counterparty.m_m);
     if (amount_xts > user_balance_xts) {
         throw not_enough_funds_error();
     } else {
@@ -21,6 +37,8 @@ void user::transfer(user &counterparty,
         counterparty.transtactions.push_back(input_transaction);
         counterparty.balance_change(true, amount_xts);
         balance_change(false, amount_xts);
+        cond.notify_all();
+        counterparty.cond.notify_all();
     }
 }
 void user::balance_change(bool mode, int amount_xts) {
@@ -30,10 +48,11 @@ void user::balance_change(bool mode, int amount_xts) {
         user_balance_xts -= amount_xts;
     }
 }
-void user::snapshot_transactions(
+user_transactions_iterator user::snapshot_transactions(
     std::function<void(const std::vector<transaction> &transactions1,
                        int balance)> const &input) const {
     std::unique_lock l(m_m);
     input(transtactions, user_balance_xts);
+    return user_transactions_iterator(this);
 }
 }  // namespace bank
--- bank.h
+++ bank.h
@@ -1,9 +1,9 @@
+#include <condition_variable>
 #include <functional>
 #include <map>
 #include <mutex>
 #include <string>
 #include <vector>
-
 namespace bank {
 struct transfer_error : std::runtime_error {
     explicit transfer_error(std::string const &a = "transfer_error")
@@ -14,39 +14,54 @@ struct not_enough_funds_error : transfer_error {
         : transfer_error("not_enough_funds_error"){};
 };
 struct transaction;
+struct user_transactions_iterator;
 struct user {
 private:
     mutable std::mutex m_m;
     std::string username;
     int user_balance_xts = 0;
     std::vector<transaction> transtactions;
+    mutable std::condition_variable cond;
 
 public:
     user() = default;
     explicit user(std::string const &name_);
-    std::string name() const noexcept {
+    const std::string &name() const noexcept {
         return username;
     }
     int balance_xts() const {
+        std::unique_lock l(m_m);
         return user_balance_xts;
     }
-    void snapshot_transactions(
+
+    user_transactions_iterator snapshot_transactions(
         std::function<void(const std::vector<transaction> &transactions,
                            int balance)> const &input) const;
     void balance_change(bool mode, int amount_xts);
     void transfer(user &counterparty,
                   int amount_xts,
                   const std::string &comment);
+
+    friend struct user_transactions_iterator;
+    user_transactions_iterator monitor() const;
+};
+struct user_transactions_iterator {
+    explicit user_transactions_iterator(const user *user_ptr);
+    transaction wait_next_transaction();
+
+private:
+    const user *user_ptr;
+    size_t index;
 };
 struct transaction {
 public:
-    user *const
+    const user *const
         counterparty;  // NOLINT(misc-non-private-member-variables-in-classes)
     const int
         balance_delta_xts;  // NOLINT(misc-non-private-member-variables-in-classes)
     const std::string
         comment;  // NOLINT(misc-non-private-member-variables-in-classes)
-    transaction(user *const name, int balance, std::string coment)
+    transaction(const user *const name, int balance, std::string coment)
         : counterparty(name),
           balance_delta_xts(balance),
           comment(std::move(coment)) {
@@ -61,6 +76,9 @@ private:
 public:
     user &get_or_create_user(const std::string &name) {
         std::unique_lock l(m_m);
+        if (users.find(name) != users.end()) {
+            return users[name];
+        }
         users.emplace(name, name);
         return users[name];
     }
